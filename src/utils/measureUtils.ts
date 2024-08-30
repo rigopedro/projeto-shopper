14667340e91f8badad32568a136436d3509e62fc
Pedro Rigo <pedrorigooliveira@hotmail.com>
2024-08-29 21:32:15 -0300

    Terminar essa regra aqui depois

--- src/utils/measureUtils.ts
+++ src/utils/measureUtils.ts
@@ -1,9 +1,9 @@
 
 export const isMeasureDuplicate = (costumer_code: string, measure_datetime: string, measure_type: string): boolean => {
-    return false; // Placeholder
+    return false; 
   };
   
   export const isMeasuredConfirmed = (measure_uuid: string): boolean => {
-    return false; // Placeholder
+    return false; 
   };
   
